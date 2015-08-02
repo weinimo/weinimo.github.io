4ad3c96b71a338f317076df41796cb6556150e2b
Thomas Weininger <devel@weininger.net>
2015-08-02 11:44:55 +0200

    Add plugins to pelican configuration

--- pelicanconf.py
+++ pelicanconf.py
@@ -42,3 +42,9 @@ DEFAULT_PAGINATION = 10
 
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
+
+PLUGIN_PATHS = ['/home/tom/workspace/pelican-plugins']
+PLUGINS = ['sitemap', 'gravatar']
+SITEMAP = {
+    'format': 'xml',
+}
